080f44bdf0feb781926077b5ff7eddf45340ff6a
Juan Morales <mcallister2696@gmail.com>
2022-02-27 15:55:19 -0500

    primer pagina inicial y menu

---
+++ Formularios/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AmbientesSena</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+    <?php include_once "./views/menu.php"; ?>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Instructores</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Competencias</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Programas</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Fichas</a>
+        </li>
+    </ul>
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ Formularios/views/menu.php
@@ -0,0 +1,21 @@
+<?php
+    define ('APP_URL', 'http://localhost/repositorios/AmbientesSena');// URL de PC en casa
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./../index.php">
+            <img src="<?php echo APP_URL;?>/Formularios/views/logo-sena.jpg" width="100" height="100" alt="logosena">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Ver Horarios</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
